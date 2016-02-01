6508ba07a2501a83b3124419af0fff38169ac877
Paul Peregud <paulperegud@gmail.com>
2016-02-01 23:04:53 +0100

    extend model with more detailed data and add very basic test

--- src/ecbrates/models.py
+++ src/ecbrates/models.py
@@ -5,10 +5,18 @@ from django.db import models
 # Create your models here.
 
 class ECBRate(models.Model):
+    # _key = models.CharField("primary key, combination of published and code",
+    #                         max_length=50, primary_key=True)
     timestamp = models.DateTimeField(auto_now_add=True, null=False)
-    published = models.DateTimeField()
-    code = models.CharField(max_length=3, null=False)
-    rate = models.DecimalField(max_digits=30, decimal_places=4, null=False)
+    date = models.DateTimeField("publication date, as defined by ECB")
+    code = models.CharField("target currency code", max_length=3, null=False)
+    rate = models.DecimalField("exchange rate", max_digits=30, decimal_places=4, null=False)
+
+    @classmethod
+    def create(arate, date, code, rate):
+        # key = date ++ code
+        return arate(date = date, code = code, rate = rate# , _key = key
+        )
 
     def __unicode__():
         return self.code
--- src/ecbrates/tests.py
+++ src/ecbrates/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
 
+from .models import ECBRate
+from datetime import datetime
+from pytz import timezone
+
 # Create your tests here.
+
+class ECBRateTestCase(TestCase):
+    def setUp(self):
+        Warsaw =timezone('Europe/Warsaw')
+        ECBRate.objects.create(date=datetime(2016,1,1,0,0,tzinfo=Warsaw), code="USD", rate="1.01")
+        ECBRate.objects.create(date=datetime(2016,1,2,0,0,tzinfo=Warsaw), code="USD", rate="1.009")
+
+    def test_rate_creation(self):
+        """objects can be created and retrieved"""
+        allusd = ECBRate.objects.all()
+        self.assertEqual(2, len(allusd))
+
+    def test_rate_search(self):
+        """objects can be created and using key"""
+        Warsaw =timezone('Europe/Warsaw')
+        ARate = ECBRate.objects.get(rate="1.01")
+        self.assertEqual(datetime(2016,1,1,0,0,tzinfo=Warsaw), ARate.date)
