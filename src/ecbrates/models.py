0a2e119bce0b9beb1c2e0e2ff4ee1f3d1385c065
Paul Peregud <paulperegud@gmail.com>
2016-02-01 20:52:11 +0100

    add model for rates

--- src/ecbrates/models.py
+++ src/ecbrates/models.py
@@ -3,3 +3,12 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class ECBRate(models.Model):
+    timestamp = models.DateTimeField(auto_now_add=True, null=False)
+    published = models.DateTimeField()
+    code = models.CharField(max_length=3, null=False)
+    rate = models.DecimalField(max_digits=30, decimal_places=4, null=False)
+
+    def __unicode__():
+        return self.code
